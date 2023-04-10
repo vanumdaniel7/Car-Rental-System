a497a4cc083d382c6afd948556e302871f2e16d1
vanumdaniel7 <vanumdaniel7@gmail.com>
2023-04-11 03:27:57 +0530

    Final App

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const db = require("./db/index.js");
+const adminRoutes = require("./routes/adminRoutes.js");
+const authRoutes = require("./routes/authRoutes.js");
+const carRoutes = require("./routes/carRoutes.js");
+const app = express();
+const PORT = 3000;
+
+db.connect();
+app.use(express.json());
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/cars", carRoutes);
+app.use(express.static(path.join(__dirname, "./client/build/")));
+app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "./client/build/index.html"))});
+app.listen(PORT);
